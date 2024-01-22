437d3262dd6dff51750f401d7ee76c28fd9312c8
Renato Nazzari <renatomlnz@gmail.com>
2024-01-21 22:15:54 -0300

    Utilizando variáveis

--- 1-meuPrimeiroPrograma.js
+++ 1-meuPrimeiroPrograma.js
@@ -1,1 +1,17 @@
-console.log("Meu primeiro programa JS");
+console.log("Meu primeiro programa JS: trabalhando com variáveis");
+
+//Atribuição de uma variável
+const idade = 30;
+
+// Uso de uma variável
+console.log("Idade: " + idade);
+
+// Uso de uma variável em uma operação aritmética
+console.log(idade + 2);
+console.log(idade - 2);
+console.log(idade * 2);
+console.log(idade / 2);
+
+// Atribuindo uma variável com uma já existente
+const idadeSomada = idade + 2;
+console.log(idadeSomada);
